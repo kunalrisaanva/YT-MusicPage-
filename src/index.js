d77f3e3c387573b62020d6604016c25b4cf1efdc
kunal <kunalrisaanva12@gmail.com>
2024-04-01 13:57:30 +0530

    update files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path"
+
+const PORT = 2000
+
+const app = express()
+
+
+
+
+app.use(express.static("public"));
+
+
+
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__filename,"./index.html"));
+
+})
+
+
+
+
+app.listen(2000,() =>   `server is running on port ${PORT}`)
