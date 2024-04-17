9fc64d5c4d6e157906cf80964eb98b8de91af86a
kunal <kunalrisaanva12@gmail.com>
2024-04-17 11:04:21 +0530

    complate

--- index.js
+++ index.js
@@ -2,23 +2,29 @@
 let arr = [
 
     {
-        songName:"Less then zero",url:"./songs/Y2meta.app - The Weeknd - Less Than Zero (Official Lyric Video) (128 kbps).mp3",img:"./images/dawn fm - the weekend.jpeg"
+        songName:"Less then zero",url:"./songs/Y2meta.app - The Weeknd - Less Than Zero (Official Lyric Video) (128 kbps).mp3",img:"./images/3.jpeg"
     },
     {
         songName:"StarBoy",url:"./songs/Starboy (Speed Up Reverb)-(PagalSongs.Com.IN).mp3",img:"./images/_.jpeg"
     },
     {
-        songName:"Pahle bhi main",url:"./songs/Pehle Bhi Main - Animal 128 Kbps.mp3",img:"./images/Explore a captivating collection of high-definition animal movie wallpapers on Pinterest!.jpeg"
+        songName:"Pahle bhi main",url:"./songs/Pehle Bhi Main - Animal 128 Kbps.mp3",img:"./images/4.jpeg"
     },
     {
-        songName:"After Hour",url:"./songs/The_Weeknd_-_After_Hours.mp3",img:"./images/after hours - the weeknd poster.jpeg"
+        songName:"After Hour",url:"./songs/The_Weeknd_-_After_Hours.mp3",img:"./images/afterHour2.jpeg"
     },
 
 
 ]
 
 
+const audio = new Audio()
+let selectedSong = 0;
 
+let poster = document.querySelector("#left")
+let play = document.querySelector("#play")
+let forward = document.querySelector("#forward")
+let backward = document.querySelector("#backward")
 
 function mainFuntion (){
     let clutter = ""
@@ -41,8 +47,14 @@ function mainFuntion (){
  <div class="border-t border-gray-600 w-[33rem] "></div> 
 
 `
-
 document.querySelector('#all-song').innerHTML = clutter
+
+audio.src = arr[selectedSong].url
+console.log(arr[selectedSong].img);
+
+poster.style.backgroundImage = `url(${arr[selectedSong].img})`
+
+
 
 })
 
@@ -54,22 +66,52 @@ mainFuntion()
 
 
 
-
-// console.log(clutter)
-
-
-const audio = new Audio()
-
-
 document.querySelector('#all-song').addEventListener("click",function(val){
     const id = val.target.id
     const int = Number(id)
    
-    audio.src = arr[int].url
+    // audio.src = arr[int].url
+    selectedSong = int
+   
+    mainFuntion()
+    play.innerHTML = `<i class="ri-pause-mini-line"></i>`
    
     audio.play()
 })
 
 
+var flag = 0
+
+play.addEventListener("click",function(val){
+    if(flag === 0 ){
+        play.innerHTML = `<i class="ri-pause-mini-line"></i>`
+        mainFuntion()
+        audio.play()
+        flag = 1
+    }else{
+        play.innerHTML = `<i class="ri-play-mini-line"></i>`
+        mainFuntion()
+        audio.paplayuse()
+        flag = 0
+    }
+})
 
 
+forward.addEventListener("click",function(val){
+    if(selectedSong < arr.length -1){
+        selectedSong++
+        mainFuntion()
+        audio.play()
+
+    }
+})
+
+
+backward.addEventListener("click",function(val){
+    if(selectedSong > 0){
+        selectedSong--
+        mainFuntion()
+        audio.play()
+
+    }
+})
